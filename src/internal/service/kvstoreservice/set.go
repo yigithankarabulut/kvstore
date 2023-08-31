a6b52bde9f354a231304d64cfd8bb18b8bcf0793
yigithankarabulut <yigithannkarabulutt@gmail.com>
2023-08-31 17:25:23 +0300

    implement service layer

---
+++ src/internal/service/kvstoreservice/set.go
@@ -0,0 +1,23 @@
+package kvstoreservice
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s *kvStoreService) Set(ctx context.Context, sr *SetRequest) (*ItemResponse, error) {
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		value, err := s.storage.Set(sr.Key, sr.Value)
+		if err != nil {
+			return nil, fmt.Errorf("kvstoreservice.Set storage.Set err: %w", err)
+		}
+
+		return &ItemResponse{
+			Key:   sr.Key,
+			Value: value,
+		}, nil
+	}
+}

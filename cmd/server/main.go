692e7c64a6eeedde7cb4876394665b53e4de00ee
yigithankarabulut <yigithannkarabulutt@gmail.com>
2023-09-01 11:49:47 +0300

    add server

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/yigithankarabulut/kvstore/src/apiserver"
+)
+
+func main() {
+	if err := apiserver.New(
+		apiserver.WithServerEnv(os.Getenv("SERVER_ENV")),
+		apiserver.WithLogLevel(os.Getenv("LOG_LEVEL")),
+	); err != nil {
+		log.Fatal(err)
+	}
+}
